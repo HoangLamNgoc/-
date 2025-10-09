52cebc528b58d2dad213aca8d7151a2f69a3f8f1
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-09 19:54:40 +0700

    Update COMPUTING.cpp

--- LNGOC & SỰ CAY CÚ CỦA CÔ ẤY/COMPUTING.cpp
+++ LNGOC & SỰ CAY CÚ CỦA CÔ ẤY/COMPUTING.cpp
@@ -14,20 +14,18 @@ int main() {
     cin >> n >> k; 
 
     int a[n], prefix[n + 1]; 
-    prefix[0] = 0;
     int best = INT_MAX,w = 0;  
-    long long sum, dem, cnt; 
-    sum = dem = cnt = 0; 
+	
+    long long sum, dem; 
+    sum = dem = 0; 
+	prefix[0] = 0;
 
     for(int i = 0; i < n; ++i) {
-        cin >> a[i];
-        sum += a[i];
+        cin >> a[i]; sum += a[i];
         dem = max(dem, (long long)a[i]); 
         prefix[i + 1] = prefix[i] + a[i];
     } 
-    for(int& i : prefix) cout << i << " "; 
-    cout << endl; 
-    cout << dem << endl;
+	
     if (k == 0) 
         cout << sum << endl; 
     else {
@@ -35,9 +33,8 @@ int main() {
         for(int i = 0; i < n; ++i) {
             if (a[i] == dem) { 
                 int k1 = min(i + k, n - 1);
-                cout << k1 << " "; 
                 int j = max(i - k, 0);
-                cout << j << endl;  
+				
                 if (prefix[k1 + 1] - prefix[j] < best) {
                     best = prefix[k1 + 1] - prefix[j];
                     w = k1 - j + 1;
@@ -45,7 +42,7 @@ int main() {
                 }  
             }
         }
-        cout << ans << endl; 
+        cout << ans << '\n'; 
     }
     return 0; 
 }
