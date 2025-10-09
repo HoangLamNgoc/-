e32f3c3bb7347cc4dd22d7ceacc690316a04968d
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-09 21:54:14 +0700

    Update CIRCLE.cpp

--- LNGOC & SỰ CAY CÚ CỦA CÔ ẤY/CIRCLE.cpp
+++ LNGOC & SỰ CAY CÚ CỦA CÔ ẤY/CIRCLE.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
-#include <map>
+#include <cmath>
+#include <iomanip>
 #include <algorithm>
-
 using namespace std;
 
-struct toado {
-    int x, y;
-    int s = x * x + y * y; 
-};
-
-bool cmp (const toado& a, const toado& b) {
-    return a.s <= b.s;
-}
-
 int main() {
-    int n; cin >> n; 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0); 
+
+    freopen("CIRCLE.INP","r", stdin);
+    freopen("CIRCLE.OUT","w", stdout); 
+
+    int n; 
+    cin >> n;
 
     const int maxn = 1e4;
-    toado a[maxn + 5], b[maxn + 5]; 
+    int sa[maxn + 5], sb[maxn + 5]; 
 
-    for(int i = 1; i <= n; ++i) cin >> a[i].x >> a[i].y;     
-    for (int i = 1; i <= n; ++i) cin >> b[i].x >> b[i].y; 
+    for(int i = 1; i <= n; ++i) {
+        int u, v; 
+        cin >> u >> v; 
+        sa[i] = u * u + v * v;
+    }
+    for(int i = 1; i <= n; ++i) {
+        int u, v; 
+        cin >> u >> v;
+        sb[i] = u * u + v * v;
+    }
 
-    sort(a + 1, a + n + 1, cmp); 
-    sort(b + 1, b + n + 1, cmp); 
+    sort(sa + 1, sa + n + 1);
+    sort(sb + 1, sb + n + 1);
 
-    int lo = 1; hi = n; 
-    while(lo <= hi) {
-      int mid = (lo + hi) / 2; 
-      upper_bound(a + 1, a + n + 1, a[mid].s); 
-      
+    int i = 1;
+    while(i <= n) {
+        int maxv = max(sa[i], sb[i]);
+        int minv = min(sa[i + 1], sb[i + 1]);
+        if(maxv < minv) {
+            cout << fixed << setprecision(6) << sqrt(max(sa[i], sb[i])) << endl;
+            return 0;
+        }
+        else i++;
+    }
+
+    cout << fixed << setprecision(6) << sqrt(max(sa[n], sb[n])) << endl;
+    return 0;
+}
