df4d2df292362f1f4460dcca4f01a79377194252
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-14 14:39:56 +0700

    Update PERMUTATION.cpp

--- LNGOC & SỰ CAY CÚ CỦA CÔ ẤY/PERMUTATION.cpp
+++ LNGOC & SỰ CAY CÚ CỦA CÔ ẤY/PERMUTATION.cpp
@@ -1,45 +1,43 @@
 #include <iostream>
-#include <algorithm>
-#include <cmath>
-
+#include <vector>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
 
     freopen("PERMUTATION.INP", "r", stdin);
-    freopen("PERMUTATION.OUT", "w", stdout); 
-    
-	int n;
-    long long k; 
+    freopen("PERMUTATION.OUT", "w", stdout);
+
+    int n;
+    long long k;
     cin >> n >> k;
-    vector<int> a; 
-    int n1 = n; 
 
-    long long fa[20]; fa[0] = 1; 
-    for(int i = 1; i <= 20; ++i) 
-        fa[i] = fa[i - 1] * i;
-    bool vis[n + 1] = {0}; 
-    vector<int> v; 
-    for(int i = 0; i <= n; ++i) v.push_back(i);  
+    vector<long long> fact(21, 1);
+    for (int i = 1; i <= 20; ++i)
+        fact[i] = fact[i - 1] * i;
 
-    for(int i = 1; i <= n1; ++i) {
-        int pos; 
-        if(n > 20) {
-            pos = 1;
-            n = 20; 
+    vector<int> v;
+    for (int i = 1; i <= n; ++i)
+        v.push_back(i);
+
+    vector<int> res;
+    int n1 = n;
+    for (int i = 1; i <= n1; ++i) {
+        int rem = n - i;
+        long long block = (rem > 20 ? fact[20] : fact[rem]);
+        int pos = (k - 1) / block;
+        k = (k - 1) % block + 1;
+
+        if (pos >= (int)v.size()) {
+            cout << -1;
+            return 0;
         }
-        else pos = (k - 1) / fa[n - i] + 1; 
-        k = (k - 1) % fa[n - i] + 1;
-        
-        if(pos > v.size()) {
-            cout << -1 << endl; 
-            return 0; 
-        }
-        a.push_back(v[pos]); 
-        v.erase(v.begin() + pos); 
+
+        res.push_back(v[pos]);
+        v.erase(v.begin() + pos);
     }
-    for(const int i : a) cout << i << " "; 
-    return 0; 
+
+    for (int x : res) cout << x << " ";
+    return 0;
 }
