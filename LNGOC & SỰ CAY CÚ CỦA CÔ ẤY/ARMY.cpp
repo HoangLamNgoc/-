2896ce21e2050c5e52fe423882d29c97b919eff5
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-11-03 14:27:08 +0700

    Update ARMY.cpp

--- LNGOC & SỰ CAY CÚ CỦA CÔ ẤY/ARMY.cpp
+++ LNGOC & SỰ CAY CÚ CỦA CÔ ẤY/ARMY.cpp
@@ -1,59 +1,55 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-const int N = (int)1e5 + 5;
+const int N = 1e5 + 5;
 vector<int> g[N];
 long long dp[N], val[N];
-bool vis[N];
-
-void dfs(int u, int par = 0) {
-    vis[u] = 1;
-    for (int v : g[u]) {
-        if (v == par) continue;
-
-        if (!vis[v]) {
-            if (val[v] < dp[u]) {
-                dp[v] = max(dp[v], dp[u]);
-            } else {
-                dp[v] = max(dp[v], dp[u] / 2);
-            }
-            dfs(v, u);
-        } else {
-            dp[v] = max(dp[v], (dp[u] > val[v]) ? dp[u] : dp[u] / 2);
-        }
-    }
-}
 
 int main() {
-    ios_base::sync_with_stdio(0); 
-    cin.tie(0); cout.tie(0); 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    freopen("ARMY.OUT", "r", stdin); 
-    freopen("ARMY.INP", "w", stdout); 
-    
+    freopen("ARMY.INP", "r", stdin); 
+    freopen("ARMY.OUT", "w", stdout); 
+
     int n, m;
     cin >> n >> m;
-
     for (int i = 0; i < m; ++i) {
         int u, v;
         cin >> u >> v;
-        g[u].emplace_back(v);
-        g[v].emplace_back(u);
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
 
-    fill_n(dp, N, 0);
-    cin >> dp[1];
-
+    long long s;
+    cin >> s;
     for (int i = 1; i <= n; ++i)
         cin >> val[i];
 
-    if (val[1] > dp[1])
-        dp[1] /= 2;
+    fill(dp, dp + n + 1, -1);
+    dp[1] = (val[1] > s ? s / 2 : s);
 
-    dfs(1);
+    priority_queue<pair<long long, int>> pq;
+    pq.push({dp[1], 1});
 
-    cout << dp[n] << '\n';
+    while (!pq.empty()) {
+        auto [e, u] = pq.top();
+        pq.pop();
+
+        if (e < dp[u]) continue; 
+
+        for (int v : g[u]) {
+            long long next = (e > val[v]) ? e : e / 2;
+            if (next > dp[v]) {
+                dp[v] = next;
+                pq.push({next, v});
+            }
+        }
+    }
+
+    cout << dp[n] << "\n";
 }
